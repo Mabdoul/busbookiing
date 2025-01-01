48733a600c6e30be194d979595fadf16b2bc156c
Mabdoul <iisimo29@gmail.com>
2025-01-02 00:57:46 +0100

    lets begin category page

--- src/pages/home_container/HomeContainer.jsx
+++ src/pages/home_container/HomeContainer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../hero/Hero'
+import Search from '../search/Search'
 
 const HomeContainer = () => {
   return (
    <>
    <Hero />
+   <Search />
    </>
   )
 }
--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -8,12 +8,59 @@ const Search = () => {
           className="grid grid-cols-3 gap-x-10 gap-y-12 items-end
         "
         >
-            <div className="">
-                <label htmlFor="from" className="block mb-2 font-mediuim">
-                    From
-                </label>
-                <select name="from" id="from" className="w-full text-neutral-800 dark:text-neutral-100 placeholder-neutral-400 dark:placeholder:text-neutral-600 appearence-none"></select>
-            </div>
+          <div className="">
+            <label htmlFor="from" className="block mb-2 font-semibold">
+              From
+            </label>
+            <select
+              name="from"
+              id="from"
+              className="w-full appearance-none text-neutral-800 dark:text-neutral-100 placeholder-neutral-400 dark:placeholder:text-neutral-600 inline-block bg-neutral-200/60 dark:bg-neutral-900/60 px-3 h-12 border-neutral-200 dark:border-neutral-900 rounded-md focus:outline-none focus:bg-neutral-100 dark:focus:bg-neutral-900"
+            >
+              <option value="">Select Location</option>
+              <option value="location1">Location 1</option>
+              <option value="location2">Location 2</option>
+              <option value="location3">Location 3</option>
+            </select>
+          </div>
+          <div className="">
+            <label htmlFor="To" className="block mb-2 font-semibold">
+              To
+            </label>
+            <select
+              name="To"
+              id="To"
+              className="w-full appearance-none text-neutral-800 dark:text-neutral-100 placeholder-neutral-400 dark:placeholder:text-neutral-600 inline-block bg-neutral-200/60 dark:bg-neutral-900/60 px-3 h-12 border-neutral-200 dark:border-neutral-900 rounded-md focus:outline-none focus:bg-neutral-100 dark:focus:bg-neutral-900"
+            >
+              <option value="">Select Location</option>
+              <option value="location1">Location 4</option>
+              <option value="location2">Location 5</option>
+              <option value="location3">Location 6</option>
+            </select>
+          </div>
+          <div className="">
+          <label htmlFor="date" className="block mb-2 font-semibold">
+              Choose a date
+            </label>
+            <input type="date" id="date" name="date" className="w-full appearance-none text-neutral-800 dark:text-neutral-100 placeholder-neutral-400 dark:placeholder:text-neutral-600 inline-block bg-neutral-200/60 dark:bg-neutral-900/60 px-3 h-12 border-neutral-200 dark:border-neutral-900 rounded-md focus:outline-none focus:bg-neutral-100 dark:focus:bg-neutral-900" />
+          </div>
+          <div className="">
+          <label htmlFor="time" className="block mb-2 font-semibold">
+              Choose a Time
+            </label>
+            <input type="time" id="time" name="time" className="w-full appearance-none text-neutral-800 dark:text-neutral-100 placeholder-neutral-400 dark:placeholder:text-neutral-600 inline-block bg-neutral-200/60 dark:bg-neutral-900/60 px-3 h-12 border-neutral-200 dark:border-neutral-900 rounded-md focus:outline-none focus:bg-neutral-100 dark:focus:bg-neutral-900" />
+          </div>
+          <div className="">
+          <label htmlFor="seat" className="block mb-2 font-semibold">
+              Choose a seats
+            </label>
+            <input type="number" id="seat" placeholder="Enter seat" name="seat" className="w-full appearance-none text-neutral-800 dark:text-neutral-100 placeholder-neutral-400 dark:placeholder:text-neutral-600 inline-block bg-neutral-200/60 dark:bg-neutral-900/60 px-3 h-12 border-neutral-200 dark:border-neutral-900 rounded-md focus:outline-none focus:bg-neutral-100 dark:focus:bg-neutral-900" />
+          </div>
+          <div className="">
+            <button className="btn w-full px-4 h-12 bg-violet-600 text-neutral-50 text-base font-normal rounded-md">
+              Check Availability
+            </button>
+          </div>
         </div>
       </div>
     </div>
